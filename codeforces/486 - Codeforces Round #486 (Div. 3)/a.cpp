6b09d8eb3fc9a82ee69e071c19ee2148f2b2c107
mrtakata <matheus.takata@gmail.com>
2019-02-13 16:45:26 -0200

    solved 486A

--- codeforces/486 - Codeforces Round #486 (Div. 3)/a.cpp
+++ codeforces/486 - Codeforces Round #486 (Div. 3)/a.cpp
@@ -1,60 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+// define macros
 #define ll long long int
-#define MAX 100
 
-typedef struct{
-    int first, second;
-} tPair;
-
-int k, n, j;
-bool has_answer = false;
-int inputs[MAX];
-tPair ans[MAX];
-
-bool find(int number){
-    for(int i = 0; i < j ; i++){
-        if (number == ans[i].first){
-            return true;
-        }
-    }
-    return false;
-}
 int main(){
 
-    // read k and n and generate arrays
-    cin >> n >> k;
+    // variable declaration
+    ll n;
 
-    // read inputs
-    for(int i = 0; i < n ; i++){
-        cin >> inputs[i];
+    // read input
+    cin >> n;
+    ll ans;
+    // print answer
+    if(n % 2 > 0){
+        cout << "-";
     }
+    cout << (ll) ceil(n/2.0) << endl;
 
-    // solver
-    j = 0;
-    for(int i = 0; i < n; i++){
-        if( !find(inputs[i]) ){
-            ans[j].first = inputs[i];
-            ans[j].second = i+1;
-            j++;
-        }
-
-        if( j == k){
-            has_answer = true;
-            break;
-        }
-    }
-
-    // answer
-    if(has_answer){
-        cout << "YES" << endl;
-        for(int i = 0; i < k; i++){
-            cout << ans[i].second << " ";
-        }
-        cout << endl;
-    }
-    else{
-        cout << "NO" << endl;
-    }
     return 0;
 }
