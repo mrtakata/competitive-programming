b2319438c0d2c5aa910cccb3bcaada4cf25fa132
mrtakata <matheus.takata@gmail.com>
2019-02-13 17:02:18 -0200

    solved 978A

--- codeforces/978 - Codeforces Round #481 (Div. 3)/a.cpp
+++ codeforces/978 - Codeforces Round #481 (Div. 3)/a.cpp
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
 #define ll long long int
-
 using namespace std;
-
-int n, vet[1000];
-
-void printVet(){
-  for(int i = 0; i < n; i++){
-    cout << vet[i] << " ";
-  }
-  cout << endl;
-}
 
 int main(){
 
   cin.tie(0);
   ios_base::sync_with_stdio(0);
-  int i = 0;
-
+  
+  int n;
   cin >> n;
-  while(i < n){
-    cin >> vet[i];
-    i++;
+  int a[n];
+  vector<int> ans;
+  set<int> hash;
+  for(int i = 0; i < n; i++){
+    cin >> a[i];
   }
 
-  printVet();
+  for(int i = n-1; i >= 0; i--){
+    if(hash.find(a[i]) == hash.end()){
+      hash.insert(a[i]);
+      ans.push_back(a[i]);
+    }
+  }
+  
+  // print answer
+  cout << ans.size() << endl;
+  for(int i = ans.size()-1; i >= 0; i--){
+    cout << ans[i] << ' ';
+  }
+  cout << endl;
   return 0;
 }
