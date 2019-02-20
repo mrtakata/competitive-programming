6a3f54b970325f53ea7e8850ae1553e76222f79f
mrtakata <matheus.takata@gmail.com>
2019-02-20 19:13:57 +0000

    Renamed not_done to readme & solved 1118C

--- codeforces/1118 - Codeforces Round #540 (Div. 3)/c.cpp
+++ codeforces/1118 - Codeforces Round #540 (Div. 3)/c.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 const int MAX = 21;
 int mat[MAX][MAX];
+map<int, int> nums;
+map<int, int> borders;
 
 void printMat(int n){
     for(int i = 0; i < n; i++){
@@ -15,7 +17,6 @@ void printMat(int n){
     }
 }
 
-map<int, int> nums;
 int main(){
 
     cin.tie(0);
@@ -31,8 +32,9 @@ int main(){
         nums[a[i]]++;
     }
     if(n % 2 == 0){
+        // filling corners 
         int i = 0, j = 0;
-        for(auto x: nums){
+        for(auto &x: nums){
             if(x.second % 4 != 0){
                 cout << "NO" << endl;
                 return 0;
@@ -49,36 +51,67 @@ int main(){
         }
     }
     else{
-        bool hasOdd = false;
-        int i = 0, j = 0;
-        for(auto x : nums){
+        int i = 0, j = 0, iCol = 0, jCol = n/2, iRow = n/2, jRow = 0;
+        bool alreadyFound = false, midColFilled = false, midRowFilled = false;
+        int currentTotal = 0, totalBorders = n * n - n - n + 1;
+
+        for(auto &x : nums){
+            // not possible if more than 1 value has odd occurrences
             if(x.second % 2 > 0){
-                if(!hasOdd){
-                    hasOdd = true;
-                    
-                    mat[n/2][n/2] = x.first;
-                    x.second--;                
-                    for(int i = 0; i < n/2; i++){
-                        mat[(n/2)-1][n/2] = mat[(n/2)+1][n/2] = x.first;                            
-                        x.second-=2;
-                    }
-                }
-                else{
+                if(alreadyFound){
                     cout << "NO" << endl;
                     return 0;
                 }
+                else{
+                    alreadyFound = true;
+                    mat[n/2][n/2] = x.first;
+                    x.second--;                  
+                }
             }
-            else{
-                if(x.second % 4 == 0 && i + j <= n/2){
-                    for(int k = 0; k < x.second / 4; k++){
-                        mat[i][j] = mat[n-1-i][j] = 
-                        mat[i][n-1-j] = mat[n-1-i][n-1-j] = x.first;
-                        i++;
-                        if(i >= n/2){
-                            i = 0; 
-                            j++;
-                        }
-                    }
+            // separate values to fill borders
+            while(x.second / 4 > 0 && currentTotal < totalBorders){
+                currentTotal += 4;
+                x.second -= 4;
+                borders[x.first] += 4;
+            }
+    
+            // fill middle values
+            if(!midRowFilled){// filling mid row
+                while(x.second > 0 && !midRowFilled){
+                    mat[iRow][jRow] = mat[iRow][n-1-jRow] = x.first;
+                    x.second-=2;
+                    jRow++;
+                    if(jRow == n/2) midRowFilled = true;
+                }
+            }
+            if(!midColFilled){  // filling mid col
+                while(x.second > 0 && !midColFilled){
+                    mat[iCol][jCol] = mat[n-1-iCol][jCol] = x.first;
+                    x.second -=2;
+                    iCol++;
+                    if(iCol == n/2) midColFilled = true;
+                }
+            }
+        }
+        // fill corners, just like n even
+        if(currentTotal != totalBorders){
+            cout << "NO" << endl;
+            return 0;
+        }
+
+        for(auto &x: borders){
+            
+            if(x.second % 4 != 0){
+                cout << "NO" << endl;
+                return 0;
+            }
+            for(int k = 0; k < x.second / 4; k++){
+                mat[i][j] = mat[n-1-i][j] = 
+                mat[i][n-1-j] = mat[n-1-i][n-1-j] = x.first;
+                i++;
+                if(i >= n/2){
+                    i = 0; 
+                    j++;
                 }
             }
         }
